62812177ba1c517e3dfa8a0b6336d9a16c055127
toubhie <tobiwily@yahoo.com>
2023-11-21 18:10:08 +0000

    exported interfaces and helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AutoSuggestComponent from './components/AutoSuggestComponent';
 
 function App() {
   const handleSelectCharacter = (character: { id: number; name: string }) => {
-    // Display an alert with the selected character's name
+    // Display the alert with the selected character's name
     alert(`Selected character: ${character?.name}`);
   };
 
   return (
     <div>
-      <h1>Marvel Character AutoSuggest</h1>
+      <h1>Marvel Characters AutoSuggest</h1>
+
       <AutoSuggestComponent onSelectCharacter={handleSelectCharacter} />
     </div>
   );
--- src/components/AutoSuggestComponent.tsx
+++ src/components/AutoSuggestComponent.tsx
@@ -1,62 +1,63 @@
 import React, { useState, ChangeEvent } from 'react';
-import axios from 'axios';
 import { fetchCharacters } from '../api/apis';
 import { getTimestamp } from '../utils/helpers';
-
-interface Character {
-  id: number;
-  name: string;
-}
+import { Character } from '../interfaces/character';
 
 interface AutoSuggestComponentProps {
-  onSelectCharacter: (character: Character) => void;
+    onSelectCharacter: (character: Character) => void;
 }
 
 const AutoSuggestComponent: React.FC<AutoSuggestComponentProps> = ({ onSelectCharacter }) => {
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [characters, setCharacters] = useState<Character[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [characters, setCharacters] = useState<Character[]>([]);
 
-  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setSearchTerm(value);
+    const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setSearchTerm(value);
 
-    // Fetch characters from Marvel API
-    if (value.length >= 2) {
-      const results = await fetchCharacters(value, getTimestamp());
+        if (value.length >= 2) {
+            const results = await fetchCharacters(value, getTimestamp());
 
-      console.log('results', results)
+            console.log('results', results)
 
-      setCharacters(results);
-    }
-  };
+            setCharacters(results);
+        }
+    };
 
 
-  const handleSelectCharacter = (character: Character) => {
-    // Pass the selected character to the parent component
-    onSelectCharacter(character);
+    const handleSelectCharacter = (character: Character) => {
+        // Clear the input field
+        setSearchTerm('');
+        setCharacters([]);
 
-    // Clear the input field
-    setSearchTerm('');
-    setCharacters([]);
-  };
+        // Pass the selected character to the parent component
+        onSelectCharacter(character);
+    };
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Type the first 2 letters of a Marvel character"
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      <ul>
-        {characters?.map((character) => (
-          <li key={character?.id} onClick={() => handleSelectCharacter(character)}>
-            {character?.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Type first 2 letters of a Marvel character"
+                value={searchTerm}
+                onChange={handleChange}
+            />
+
+            {characters.length > 0 && (
+                <div className="dropdown">
+                    {characters.map((character, index) => (
+                        <React.Fragment key={character.id}>
+                            {index > 0 && <div className="divider" />}
+                            <div className="dropdown-item" onClick={() => handleSelectCharacter(character)}>
+                                {character.name}
+                            </div>
+                        </React.Fragment>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
+
 
 export default AutoSuggestComponent;
